bb74983bbc18798313df38f96537293c598d45a7
Ayan1248 <zunainahzayan@gmail.com>
2024-10-25 18:27:28 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+var x=gsap.timeline()
+
+x.from("#nav-4",{
+    y:60,
+    opacity:0,
+    duration:1,
+    delay:0.1
+})
+x.from("#nav-2",{
+    y:-60,
+    opacity:0,
+    duration:1,
+    delay:0.1
+    
+})
+x.from("h4",{
+    y:-60,
+    opacity:0,
+    duration:1,
+    stagger:0.3
+    
+})
+
+gsap.to("#pg-2 h3",{
+    transform:"translateX(-70%)",
+    scrollTrigger:{
+        trigger:"#pg-2",
+        scroller:"body",
+        markers:true,
+        start:"top 0%",
+        end:"top -100%",
+        scrub:2,
+        pin:true
+    }
+})
